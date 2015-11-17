5b17a9a882fa9cac9418748c7a14570c7a3c8bfb
Glenn McCartney <glennmccartney@gmail.com>
2015-11-17 12:41:18 +0000

    Initial Commit

---
+++ Where Was I/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Where Was I
+//
+//  Created by Glenn McCartney on 17/11/2015.
+//  Copyright © 2015 Glenn McCartney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
