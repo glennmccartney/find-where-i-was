63d4908e9014ee504d18e2763aacd7b8e6a63f87
Glenn McCartney <glennmccartney@gmail.com>
2015-11-19 14:53:55 +0000

    got routing working

--- Where Was I/PopoverContentViewController.swift
+++ Where Was I/PopoverContentViewController.swift
@@ -6,50 +6,91 @@
 //  Copyright © 2015 Glenn McCartney. All rights reserved.
 //
 import UIKit
+import CoreLocation
+import MapKit
 
 protocol MarkedLocationDelegate {
     func updateData(data: String)
+    func deleteData(data: Bool)
+    func takeMeHere(data: MKMapItem)
 }
 
 class PopoverContentViewController: UIViewController {
 
+    
+    
     var delegate: MarkedLocationDelegate?
-    
     var originalMarkedLocationName : String?
-    
+    var markedLocationLat : String?
+    var markedLocationLng : String?
+    var MapItem : MKMapItem?
     
     @IBOutlet weak var markedLocationName: UITextField!
     
+    @IBOutlet weak var addressLabel: UILabel!
     
     @IBAction func saveButton(sender: AnyObject) {
-        //myPopoverPresentationController.delegate = nil
-        //myPopoverPresentationController = nil
-        //myPopoverController = nil
         
-        
-        print ("button clicked")
-        print ("value is \(markedLocationName.text)")
+        print ("button clicked. Value is \(markedLocationName.text)")
         
         self.delegate?.updateData(markedLocationName.text!)
-        
-        
-         self.performSegueWithIdentifier("unwindIdentifier", sender: self)
+        self.performSegueWithIdentifier("unwindIdentifier", sender: self)
     }
     
+    
+    @IBAction func deleteButton(sender: AnyObject) {
+        print ("delete clicked")
+        
+        //To-Do : Add an "Are You sure?" message
+        self.delegate?.deleteData(true)
+        self.performSegueWithIdentifier("unwindIdentifier", sender: self)
+    }
+    
+    
+    @IBAction func takeMeHereButton(sender: AnyObject) {
+        self.delegate?.takeMeHere(MapItem!)
+        self.performSegueWithIdentifier("unwindIdentifier", sender: self)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
         
         if let omln = originalMarkedLocationName {
-        
             markedLocationName.text = omln
-            
         }
         else {
             print("empty selection")
         }
-
+        
+        
+        //Reverse Geocode
+        
+        print (markedLocationLat)
+        print (markedLocationLng)
+        
+        let myDoubleLat = Double(markedLocationLat!)
+        let myDoubleLng = Double(markedLocationLng!)
+        
+        
+         let location = CLLocation(latitude : myDoubleLat!, longitude: myDoubleLng!)
+        
+        CLGeocoder().reverseGeocodeLocation(location,
+            completionHandler: {(placemarks:[CLPlacemark]?, error:NSError?) -> Void in
+                if let placemarks = placemarks {
+                    let placemark = placemarks[0]
+                    self.addressLabel.text = self.formatAddressFromPlacemark(placemark)
+                    
+                    self.MapItem = MKMapItem(placemark:  MKPlacemark(coordinate: placemark.location!.coordinate,
+                        addressDictionary: placemark.addressDictionary as! [String:AnyObject]?))
+                }
+        })
+        
+    }
+    
+    func formatAddressFromPlacemark(placemark: CLPlacemark) -> String {
+        return (placemark.addressDictionary!["FormattedAddressLines"] as!
+            [String]).joinWithSeparator(", ")
     }
     
 }
--- Where Was I/ViewController.swift
+++ Where Was I/ViewController.swift
@@ -17,46 +17,33 @@ class MKPointAnnotationCustom: MKPointAnnotation  {
 
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
-    var internalData : String?
     var locationManager = CLLocationManager()
     var marklocation = CLLocationCoordinate2D()
     var locationPointAnnotation = MKPointAnnotation()
     var userZoom : Double = 0
+    var circle : MKCircle?
     
     var markedPointAnnotations = [MKPointAnnotationCustom()]
     var currentSelectedMarkedPointAnnotation = MKPointAnnotationCustom()
    
+    var locationTuples: [(textField: UITextField!, mapItem: MKMapItem?)]!
+    
+    var currentLocation : CLLocationCoordinate2D?
+    
+    var sourceMapItem : MKMapItem?
+    
+    var displayedPolyline : MKOverlay?
+    
     
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var myMapView: MKMapView!
-    
-    
+    @IBOutlet weak var addressLabel: UILabel!
     
     
     @IBAction func returned(segue: UIStoryboardSegue)
     {
         print("Segue Unwound")
-        print("internalData=\(internalData)")
         
-        
-        markedPointAnnotations.removeAtIndex(currentSelectedMarkedPointAnnotation.userData! - 1)
-        
-        let marklocation : CLLocationCoordinate2D = currentSelectedMarkedPointAnnotation.coordinate
-        
-        
-        myMapView.removeAnnotation(currentSelectedMarkedPointAnnotation)
-        
-        
-        let pointAnnotation = MKPointAnnotationCustom()
-        pointAnnotation.coordinate = marklocation
-        pointAnnotation.title = internalData
-        pointAnnotation.userData = markedPointAnnotations.count
-            
-        myMapView.addAnnotation(pointAnnotation)
-        
-        markedPointAnnotations.append(pointAnnotation)
-        
-    
     }
     
     @IBAction func MarkLocationButtonTapped(sender: AnyObject) {
@@ -73,16 +60,13 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
         let pointAnnotation = MKPointAnnotationCustom()
         pointAnnotation.coordinate = marklocation
-        pointAnnotation.title = "Marked Location"
+        pointAnnotation.title = "Marked Location \(markedPointAnnotations.count )"
         pointAnnotation.userData = markedPointAnnotations.count
             
         myMapView.addAnnotation(pointAnnotation)
             
         markedPointAnnotations.append(pointAnnotation)
     }
-    
-    
-    
     
     
     override func viewDidLoad() {
@@ -108,10 +92,6 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
             statusLabel.text = "Searching For Your Location..."
         }
         
-        
-        
-        
-        
     }
     
     override func didReceiveMemoryWarning() {
@@ -125,40 +105,58 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
     {
         
-        let location = CLLocationCoordinate2D(latitude : locations[locations.count - 1].coordinate.latitude, longitude: locations[locations.count - 1].coordinate.longitude)
+        currentLocation = CLLocationCoordinate2D(latitude : locations.last!.coordinate.latitude, longitude: locations.last!.coordinate.longitude)
         
         
         if let ha  = manager.location?.horizontalAccuracy
         {
             
-            let myRegion = MKCoordinateRegionMakeWithDistance(location, ha , ha)
+            let myRegion = MKCoordinateRegionMakeWithDistance(currentLocation!, ha , ha)
             
             myMapView.setRegion(myRegion, animated: true)
             
-            //print (location)
-            
-            
-            
-            
+  
             if locationPointAnnotation.title != nil
             {
                 myMapView.removeAnnotation(locationPointAnnotation)
             }
             
-            locationPointAnnotation.coordinate = location
+            locationPointAnnotation.coordinate = currentLocation!
             locationPointAnnotation.title = "Your Location"
            
             myMapView.addAnnotation(locationPointAnnotation)
             
+     
+            
+           
+            
+            //MKCircle
+            
+            if circle?.coordinate != nil
+            {
+                myMapView.removeAnnotation(circle!)
+            }
+            circle = MKCircle(centerCoordinate: currentLocation!, radius: 10)
+            //myMapView.addOverlay(circle!)
+            
+            
+            
+            //Reverse Geocode
+            CLGeocoder().reverseGeocodeLocation(locations.last!,
+                completionHandler: {(placemarks:[CLPlacemark]?, error:NSError?) -> Void in
+                    if let placemarks = placemarks {
+                        let placemark = placemarks[0]
+                        self.addressLabel.text = self.formatAddressFromPlacemark(placemark)
+                        
+                        self.sourceMapItem = MKMapItem(placemark:  MKPlacemark(coordinate: placemark.location!.coordinate,
+                                addressDictionary: placemark.addressDictionary as! [String:AnyObject]?))
+                    }
+            })
             
             
             
             statusLabel.text = "Found Your Location"
-            //locationManager.stopUpdatingLocation()
-            
-            //MKCircle
-            let circle = MKCircle(centerCoordinate: location, radius: 10)
-            myMapView.addOverlay(circle)
+            locationManager.stopUpdatingLocation()
             
         }
         
@@ -214,20 +212,14 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
         return pinView!
     }
-
-    
-    
     
     func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl)
     {
-        
         print("edit marked location")
         
         currentSelectedMarkedPointAnnotation = view.annotation as! MKPointAnnotationCustom
         self.performSegueWithIdentifier("ShowPopoverFromPin", sender: self)
     }
-        
-
     
     
     func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer
@@ -235,13 +227,20 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         if overlay is MKPolyline
         {
             
+            /*
             let route = overlay as! MKPolyline
             let routeRenderer = MKPolylineRenderer(polyline: route)
             routeRenderer.strokeColor = UIColor.redColor().colorWithAlphaComponent(0.6)
             let pattern = [2, 5]
             routeRenderer.lineDashPattern = pattern
             routeRenderer.lineWidth = 3
-            return routeRenderer
+            */
+            
+            let polylineRenderer = MKPolylineRenderer(overlay: overlay)
+            polylineRenderer.strokeColor = UIColor.greenColor().colorWithAlphaComponent(0.75)
+            polylineRenderer.lineWidth = 5
+            
+            return polylineRenderer
         }
         else if overlay is MKCircle
         {
@@ -256,7 +255,21 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
     }
     
-    
+    func plotPolyline(route: MKRoute) {
+       
+        // If the polyline has been drawn previously, remove it
+        if displayedPolyline != nil
+        {
+            myMapView.removeOverlay(displayedPolyline!)
+        }
+        
+        displayedPolyline = route.polyline
+        
+        myMapView.addOverlay(displayedPolyline!)
+        
+        myMapView.setVisibleMapRect(route.polyline.boundingMapRect,  edgePadding: UIEdgeInsetsMake(10.0, 10.0, 10.0, 10.0), animated: true)
+  
+    }
     
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
         print (error.description)
@@ -272,17 +285,131 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
             let myPopoverController = segue.destinationViewController
         
             myPopoverController.setValue(currentSelectedMarkedPointAnnotation.title!, forKey : "originalMarkedLocationName")
+           
+            myPopoverController.setValue(currentSelectedMarkedPointAnnotation.coordinate.latitude.description, forKey : "markedLocationLat")
+            
+            myPopoverController.setValue(currentSelectedMarkedPointAnnotation.coordinate.longitude.description, forKey : "markedLocationLng")
             
             (segue.destinationViewController as! PopoverContentViewController).delegate = self
         
         }
     }
     
+    func formatAddressFromPlacemark(placemark: CLPlacemark) -> String {
+        return (placemark.addressDictionary!["FormattedAddressLines"] as! [String]).joinWithSeparator(", ")
+    }
+    
 }
 
 extension ViewController: MarkedLocationDelegate {
         func updateData(data: String) {
-            self.internalData = data
+            
+            //self.internalData = data
+            markedPointAnnotations.removeAtIndex(currentSelectedMarkedPointAnnotation.userData! - 1)
+            
+            let marklocation : CLLocationCoordinate2D = currentSelectedMarkedPointAnnotation.coordinate
+            
+            
+            myMapView.removeAnnotation(currentSelectedMarkedPointAnnotation)
+            
+            
+            let pointAnnotation = MKPointAnnotationCustom()
+            pointAnnotation.coordinate = marklocation
+            pointAnnotation.title = data
+            pointAnnotation.userData = markedPointAnnotations.count
+            
+            myMapView.addAnnotation(pointAnnotation)
+            
+            markedPointAnnotations.append(pointAnnotation)
         }
+    
+    
+    
+    func deleteData(boolDelete: Bool) {
+        if boolDelete{
+            
+            print("deleting...")
+            markedPointAnnotations.removeAtIndex(currentSelectedMarkedPointAnnotation.userData! - 1)
+            myMapView.removeAnnotation(currentSelectedMarkedPointAnnotation)
+            
+        }
+    }
+    
+    func takeMeHere(TakeMeHere: MKMapItem)
+    {
+         print("take me here...")
+            // From     currentSelectedMarkedPointAnnotation.coordinate
+            // To       currentLocation!
+        
+        calculateSegmentDirections(sourceMapItem!, destMapItem: TakeMeHere)
+        
+        
+    }
+    
+    
+    func calculateSegmentDirections(sourceMapItem: MKMapItem, destMapItem: MKMapItem) {
+        
+        var time: NSTimeInterval = 0
+        
+        // 1
+        let request: MKDirectionsRequest = MKDirectionsRequest()
+        request.source = sourceMapItem
+        request.destination = destMapItem
+        // 2
+        request.requestsAlternateRoutes = true
+        // 3
+        request.transportType = .Walking
+        // 4
+        let directions = MKDirections(request: request)
+        directions.calculateDirectionsWithCompletionHandler ({
+            (response: MKDirectionsResponse?, error: NSError?) in
+            if let routeResponse = response?.routes {
+               
+                let quickestRouteForSegment: MKRoute = routeResponse.sort({$0.expectedTravelTime < $1.expectedTravelTime})[0]
+                
+                time += quickestRouteForSegment.expectedTravelTime
+                
+                self.showRoute(routeResponse, time: time)
+                
+                
+                
+            } else if let _ = error {
+                
+            }
+        })
+    }
+    
+    
+    func showRoute(routes: [MKRoute], time: NSTimeInterval) {
+        var directionsArray = [(startingAddress: String, endingAddress: String, route: MKRoute)]()
+        
+        for i in 0..<routes.count {
+            plotPolyline(routes[i])
+            //directionsArray += [(locationArray[i].textField.text!, locationArray[i+1].textField.text!, routes[i])]
+        }
+        
+        //displayDirections(directionsArray)
+        printTimeToLabel(time)
+    }
+    
+    func printTimeToLabel(time: NSTimeInterval) {
+        let timeString = time.formatted()
+        //totalTimeLabel.text = "Total Time: \(timeString)"
+        
+        
+        print ("Total Time: \(timeString)")
+    }
+    
+    
+}
+
+extension NSTimeInterval {
+    func formatted() -> String {
+        let formatter = NSDateComponentsFormatter()
+        formatter.unitsStyle = .Full
+        formatter.allowedUnits = [NSCalendarUnit.Hour, NSCalendarUnit.Minute, NSCalendarUnit.Second]
+        
+        return formatter.stringFromTimeInterval(self)!
+}
 }
 
